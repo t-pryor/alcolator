387e4de34ac0e73946a0975e4e76668553519bc7
Tim Pryor <tdpryor@gmail.com>
2015-03-16 19:07:46 -0400

    Storyboarding Alcolator

--- Alcolator/ViewController.h
+++ Alcolator/ViewController.h
@@ -6,6 +6,17 @@
 //  Copyright (c) 2015 Tim Pryor. All rights reserved.
 //
 
+
+
+/* NOTES
+ 
+ .h file contains only interface code
+ Interface code describes how one class communicates with another class
+ Outlets represent a connection to another class, so they belong here
+ 
+ */
+
+
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
