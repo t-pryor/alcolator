ae42883feed81818527fb1f31ee25b78402a81ee
Tim Pryor <tdpryor@gmail.com>
2015-03-23 12:32:04 -0400

    Problem with beerPercentTextFieldplacement-blocked by the navigation bar

---
+++ Alcolator/BLCMainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLCMainMenuViewController.h
+//  Alcolator
+//
+//  Created by Tim on 2015-03-19.
+//  Copyright (c) 2015 Tim Pryor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCMainMenuViewController : UIViewController
+
+@end
